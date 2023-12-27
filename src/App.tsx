c91efca22d4c8830f88f8e853df72d56f52b84e4
Jascha Tabatabai <jascha.tabatabai@gmail.com>
2023-12-27 15:28:50 +0100

    HTTP changed to get then catch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosError } from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -11,17 +11,10 @@ function App() {
     const [error, setError] = useState("");
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const res = await axios.get<User[]>(
-                    "https://jsonplaceholder.typicode.com/users"
-                );
-                setUsers(res.data);
-            } catch (err) {
-                setError((err as AxiosError).message);
-            }
-        };
-        fetchUsers();
+        axios
+            .get<User[]>("https://jsonplaceholder.typicode.com/users")
+            .then((response) => setUsers(response.data))
+            .catch((err) => setError(err.message));
     }, []);
 
     return (
